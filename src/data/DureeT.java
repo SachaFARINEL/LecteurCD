41e7cf8bb68cfb769e75aa7a7fda19ae22d436e6
Sacha FARINEL <97121186+SachaFARINEL@users.noreply.github.com>
2022-10-05 22:10:45 +0200

    fix test github desktop

--- src/data/DureeT.java
+++ src/data/DureeT.java
@@ -10,7 +10,6 @@ public class DureeT {
         testCompareA();
         testEnTexte();
         testGetLeTemps();
-        System.out.println("test");
     }
 
     private static void testConstruct1() {
